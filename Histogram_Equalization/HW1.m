3222c35ed3869e8793409c832e9743d440e39a59
Jae Yoon Chung <90415099+jcdino@users.noreply.github.com>
2021-12-26 17:40:37 -0500

    Update HW1.m

--- Histogram_Equalization/HW1.m
+++ Histogram_Equalization/HW1.m
@@ -1,16 +1,5 @@
 close all; clear all; clc;
 
-im1 = imread('imageA.png');
-im2 = imread('imageB.png');
-alpha = 0.5;
-
-result = blend(im1, im2, alpha);
-
-imwrite(result, 'result_blend.png');
-
-%%
-close all; clear all; clc;
-
 im = imread('Unequalized_Hawkes_Bay_NZ.jpg');
 n = 256;
 
