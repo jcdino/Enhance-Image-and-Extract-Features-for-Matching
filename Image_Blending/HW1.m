c8b375483ed57f256db99a6347bfa63a78335cbf
Jae Yoon Chung <90415099+jcdino@users.noreply.github.com>
2021-12-26 17:35:17 -0500

    Update HW1.m

--- Image_Blending/HW1.m
+++ Image_Blending/HW1.m
@@ -7,12 +7,3 @@ alpha = 0.5;
 result = blend(im1, im2, alpha);
 
 imwrite(result, 'result_blend.png');
-
-%%
-close all; clear all; clc;
-
-im = imread('Unequalized_Hawkes_Bay_NZ.jpg');
-n = 256;
-
-result = histEq(im, n);
-imwrite(result, 'result_histEq.png');
